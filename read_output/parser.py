daab3f7eaf74243da1da074c88defd1693dfacca
musil <felix.musil@epfl.ch>
2017-09-14 20:19:08 +0200

    added parsing lib

---
+++ read_output/parser.py
@@ -0,0 +1,96 @@
+import re,os,sys
+import numpy as np
+
+
+
+def get_filenames(path,fn_pattern='qe.out',dir_pattern=None):
+    '''
+    get all the filenames within path having fn_pattern (and dir_pattern)
+    :param path: 
+    :param fn_pattern: 
+    :param dir_pattern: 
+    :return: list of filenames with absolute path
+    '''
+    import fnmatch
+    import os
+    matches = []
+    if dir_pattern is None:
+        # Find the paths of all the filename with fn_pattern within path
+        for root, dirnames, filenames in os.walk(path):
+            for filename in fnmatch.filter(filenames, fn_pattern):
+                matches.append(os.path.join(root, filename))
+        return matches
+    else:
+        # Find the paths with dir_pattern and all the filename with fn_pattern within dir_pattern (no recursion)
+        for root, dirnames, filenames in os.walk(path):
+            for dirname in fnmatch.filter(dirnames, dir_pattern):
+                filenames = os.listdir(os.path.join(root,dirname))
+                for filename in fnmatch.filter(filenames, fn_pattern):
+                    matches.append(os.path.abspath(os.path.join(root,dirname, filename)))
+
+
+def get_patterns(fn, str_patterns):
+    '''
+    grep str_patterns in file fn
+    
+    :param fn: 
+    :param str_patterns: 
+    :return: dic (key,val)->(str_patterns,list of found lines)
+    '''
+    matches = {key: [] for key in str_patterns}
+
+    for line in open(fn, 'r'):
+        for str_pattern in str_patterns:
+            if re.search(str_pattern, line):
+                matches[str_pattern].append(line.replace('\n', ''))
+    return matches
+
+def fn2num(fn,pattern="sg_(.*)-f_"):
+    ms = re.search(pattern,fn)
+    num = int(ms.group(1))
+    return num
+
+def fn2info(fn):
+    '''
+    Find informations in absolute filename
+    :param fn: 
+    :return: dic of information, e.g. {'sg':3,'f':0}
+    '''
+    dirnames = fn.split('/')
+    for dirname in dirnames:
+        if re.search('sg_',dirname) and re.search('f_',dirname):
+            dic = {}
+            for info in dirname.split('-'):
+                if re.search('_',info):
+                    name,num = info.split('_')
+                    num = int(num)
+                    dic[name] = num
+    return dic
+
+def extract_floats(s):
+    '''
+    get list of floats from a string
+    
+    :param s: 
+    :return: 
+    '''
+    l = []
+    for t in s.split():
+        try:
+            l.append(float(t))
+        except ValueError:
+            pass
+    return l
+
+def finishedProperly(fn):
+    '''
+    Check is the qe.out finished properly
+    :param fn: 
+    :return: 
+    '''
+    str_patterns = ['JOB DONE.']
+    s = get_patterns(fn,str_patterns)['JOB DONE.']
+    if s:
+        return True
+    else:
+        return False

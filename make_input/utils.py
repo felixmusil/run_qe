a44b72e3e820f641e2a9cae7154b312cf5447d54
musil <felix.musil@epfl.ch>
2017-11-05 19:25:12 +0100

    Sg -13 implemented

--- make_input/utils.py
+++ make_input/utils.py
@@ -234,3 +234,40 @@ def change_submission(fn, replace_dict=None, add_dict=None):
 
     source.close()
     destination.close()
+
+def convert_qe_time_to_sec(timestr):
+    """
+    Given the walltime string of Quantum Espresso, converts it in a number of
+    seconds (float).
+    """
+    rest = timestr.strip()
+
+    if 'd' in rest:
+        days, rest = rest.split('d')
+    else:
+        days = '0'
+
+    if 'h' in rest:
+        hours, rest = rest.split('h')
+    else:
+        hours = '0'
+
+    if 'm' in rest:
+        minutes, rest = rest.split('m')
+    else:
+        minutes = '0'
+
+    if 's' in rest:
+        seconds, rest = rest.split('s')
+    else:
+        seconds = '0.'
+
+    if rest.strip():
+        raise ValueError("Something remained at the end of the string '{}': '{}'"
+                         .format(timestr, rest))
+
+    num_seconds = (
+        float(seconds) + float(minutes) * 60. +
+        float(hours) * 3600. + float(days) * 86400.)
+
+    return num_seconds
